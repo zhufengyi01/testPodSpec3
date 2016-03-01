30a48ec86e4f79cce642c787e2b5cac2c7bd5e73
朱封毅 <673229963@qq.com>
2016-03-01 11:41:12 +0800

    增加test 文件

---
+++ Classes/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  testPodSpec3
+//
+//  Created by 朱封毅 on 01/03/16.
+//  Copyright © 2016年 tairan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ Classes/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  testPodSpec3
+//
+//  Created by 朱封毅 on 01/03/16.
+//  Copyright © 2016年 tairan. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
